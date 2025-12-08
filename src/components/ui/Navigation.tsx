2acf947fc457e33e3c81a4242ec0b7b2d24af6d7
Bramcr <crBram98@gmail.com>
2025-12-08 19:55:50 +0100

    disable user select in nav

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <nav className="absolute top-0 left-0 right-0 z-50 py-4 mx-4 sm:mx-8 md:mx-12">
+    <nav className="absolute top-0 left-0 right-0 z-50 py-4 mx-4 sm:mx-8 md:mx-12 select-none">
       <div className="flex items-center justify-between bg-[#DBDBDB]/30 backdrop-blur-sm rounded-[16px] border border-[#E9E9E9]/30 px-4">
         <Link to="/" className="flex items-center gap-2">
           <img
